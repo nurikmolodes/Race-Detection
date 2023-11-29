b3bb3f2c923744799dd4c5e17527260676adaaa0
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2023-11-29 20:35:14 +0600

    yeah

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,51 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./App.css"; // Import your custom styles
 
+const compressImage = async (file) => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.src = event.target.result;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const maxWidth = 800; // Set your desired maximum width
+        const maxHeight = 600; // Set your desired maximum height
+        let width = img.width;
+        let height = img.height;
+
+        if (width > height) {
+          if (width > maxWidth) {
+            height *= maxWidth / width;
+            width = maxWidth;
+          }
+        } else {
+          if (height > maxHeight) {
+            width *= maxHeight / height;
+            height = maxHeight;
+          }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+
+        ctx.drawImage(img, 0, 0, width, height);
+
+        canvas.toBlob(
+          (blob) => {
+            resolve(new File([blob], file.name, { type: "image/jpeg", lastModified: Date.now() }));
+          },
+          "image/jpeg",
+          0.8,
+        ); // Adjust the quality as needed (0.8 means 80% quality)
+      };
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
+
 const CustomLoader = () => (
   <div className="custom-loader">
     <div className="loader-circle"></div>
@@ -19,8 +64,9 @@ const App = () => {
   const [apiKey, setApiKey] = useState("hfv1khnr90teta0eoa73blheuc");
   const [apiSecret, setApiSecret] = useState("4epli6g6d2g13rd8t5c2fc1dn4");
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+  const handleFileChange = async (event) => {
+    const compressedFile = await compressImage(event.target.files[0]);
+    setFile(compressedFile);
   };
 
   const handleUpload = async () => {
